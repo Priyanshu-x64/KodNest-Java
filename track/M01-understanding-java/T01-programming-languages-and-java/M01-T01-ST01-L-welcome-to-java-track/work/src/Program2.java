1f89b122cc9540506ed32c62849efd4228189fe7
Priyanshu-x64 <priyanshu.desk@gmail.com>
2026-09-04 23:09:05 +0530

    Done the 2 programs which were give as 'HW'

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Program2.java
@@ -0,0 +1,9 @@
+public class Program2{
+    public static void main(String[] args){
+        // My placement goal
+        System.out.println("---MY PLACEMENT GOAL ---");
+        System.out.println("Role: Software Developer");
+        System.out.println("Target: Get Placed");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
